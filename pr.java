6f6d8232b02940dc4d748c6495a215e3b050f892
nazzar28 <talantbekovnazar28@gmail.com>
2023-12-01 08:25:09 +0600

    third commit

--- pr.java
+++ pr.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Random;
 public class pr{
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
+    Random ran = new Random();
     int[][]f=new int[7][7];
 
     for(int i=0;i<7;i++){
@@ -13,33 +15,48 @@ public class pr{
 
     System.out.println();
 
-    for(int k=0;k<4;k++){
-      f[rx()][ry()]=1;
-      System.out.println();
-      for(int i=0;i<7;i++){
-        System.out.println();
-        for(int j=0;j<7;j++){
-          System.out.print(f[i][j]+" ");
-      }
-    }
-  }
-    
+    threebb(ran, f);
   }
 
-  private static int rx(){
-    int random=(int)(Math.random()*7);
+  private static void threebb(Random ran,int[][]f){
+    int fn=rx(ran);
+    int sn=ry(ran);
+    f[fn][sn]=1;
+    if(cs(ran)==1){
+      f[fn+1][sn]=1;
+      f[fn-1][sn]=1;
+    }
+    else{
+      f[fn][sn+1]=1;
+      f[fn][sn-1]=1;
+    }
+    for(int i=0;i<7;i++){
+      System.out.println();
+      for(int j=0;j<7;j++){
+        System.out.print(f[i][j]+" ");
+      }
+    }
+  } 
+
+  private static int cs(Random ran){
+    int hn = ran.nextInt(3-1);
+    return hn;
+  }
+
+  private static int rx(Random ran){
+    int random=ran.nextInt(8-0);
     int x=random;
     return x;
   }
 
-  private static int ry(){
-    int random=(int)(Math.random()*7);
+  private static int ry(Random ran){
+    int random=ran.nextInt(8-0);
     int y=random;
     return y;
   }
 
-  private static int random(){
-    int random=(int)(Math.random()*7);
+  private static int random(Random ran){
+    int random=ran.nextInt(8-0);
     return random;
   }
 }
